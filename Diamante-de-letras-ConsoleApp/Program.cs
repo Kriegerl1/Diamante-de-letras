7206f2477940da471d7201c7776db9ab69d5edcb
Leonardo Rodrigues <lthkrieger@gmail.com>
2024-03-31 18:19:23 -0300

    Fixed all bugs Updated Calc

--- Diamante-de-letras-ConsoleApp/Program.cs
+++ Diamante-de-letras-ConsoleApp/Program.cs
@@ -8,78 +8,55 @@
                 Console.WriteLine("Diamante de Letras | Academia de Programação 2024!\n");
 
                 string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                string letra = obterValor<string>("Digite uma letra de A a Z: ");
+                string letra = obterValor<string>("Digite uma letra para gerar o diamante: ");
 
                 char[] alfabetoArray = alfabeto.ToCharArray(0, alfabeto.Length); // Array de LETRAS [A , B , C ...];
 
 
-                decimal letraIndex = alfabeto.IndexOf(letra); // Tamanho do Array = 26;
-
-                decimal A = Math.Floor(letraIndex * 2); // Metade do letraIndex, arredondado para baixo;
+                int letraIndex = alfabeto.IndexOf(letra); // Tamanho do Array = 26;
 
 
                 #region Topo do diamante de letras
                 for (int i = 0; i <= letraIndex; i++) // altura do diamante, base da letra escrita;
                 {
-                    for (int j = 1; j <= A; j++)
+                    for (int j = 0; j < letraIndex - i; j++)
                     {
-                        if ((A - j) / 2 >= i) // espaços inicias de fora topo;
+                        Console.Write(" ");
+                    }
+                    Console.Write(alfabetoArray[i]);
+                    if (i > 0)
+                    {
+                        for (int j = 0; j < 2 * i - 1; j++)
                         {
                             Console.Write(" ");
                         }
-                        else if ((A - j) >= (2 * i) - 1)
-                        {
-                            Console.Write(alfabetoArray[i]); // gera os primeiros caracteres;
-                        }
-                        else
-                        {
-                            Console.Write(" "); // gera 1/4 do LOZANGO;
-                        }
+                        Console.Write(alfabetoArray[i]);
 
                     }
-                    for (int j = 1; j <= A; j++)// gera 1/4 do LOZANGO;
-                    {
-                        if ((1 + A - j) / 2 <= i)
-                        {
-                            Console.Write(" ");
-                        }
-
-                    }
-                    Console.WriteLine(alfabetoArray[i]); // fecha o LOZANGO;
-
+                    Console.WriteLine();
                 }
                 #endregion
 
                 // Mesma lógica, inversa do topo
                 #region Base do diamante de letras
-                for (int i = Convert.ToInt32(letraIndex) - 1; i >= 0; i--)
+                for (int i = letraIndex - 1; i >= 0; i--)
                 {
-                    for (int j = 1; j <= A; j++)
+                    for (int j = 0; j < letraIndex - i; j++)
                     {
-                        if ((A - j) / 2 >= i)
+                        Console.Write(" ");
+
+                    }
+                    Console.Write(alfabetoArray[i]);
+
+                    if (i > 0)
+                    {
+                        for (int j = 0; j < 2 * i - 1; j++)
                         {
                             Console.Write(" ");
                         }
-                        else if ((A - j + 1) / 2 >= i)
-                        {
                             Console.Write(alfabetoArray[i]);
-                        }
-                        else
-                        {
-                            Console.Write(" ");
-                        }
-
                     }
-
-                    for (int j = 1; j <= A; j++)
-                    {
-                        if ((1 + A - j) / 2 <= i)
-                        {
-                            Console.Write(" ");
-                        }
-
-                    }
-                    Console.WriteLine(alfabetoArray[i]);
+                    Console.WriteLine();
                 }
                 #endregion
 
