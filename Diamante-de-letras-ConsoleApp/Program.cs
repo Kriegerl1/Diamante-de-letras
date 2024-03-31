1f328c04b7b8c8a859fa6ae7bbe256c6da219893
Leonardo Rodrigues <lthkrieger@gmail.com>
2024-03-31 19:01:02 -0300

    Last commit

--- Diamante-de-letras-ConsoleApp/Program.cs
+++ Diamante-de-letras-ConsoleApp/Program.cs
@@ -7,59 +7,7 @@
             {
                 Console.WriteLine("Diamante de Letras | Academia de Programação 2024!\n");
 
-                string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                string letra = obterValor<string>("Digite uma letra para gerar o diamante: ");
-
-                char[] alfabetoArray = alfabeto.ToCharArray(0, alfabeto.Length); // Array de LETRAS [A , B , C ...];
-
-
-                int letraIndex = alfabeto.IndexOf(letra); // Tamanho do Array = 26;
-
-
-                #region Topo do diamante de letras
-                for (int i = 0; i <= letraIndex; i++) // altura do diamante, base da letra escrita;
-                {
-                    for (int j = 0; j < letraIndex - i; j++)
-                    {
-                        Console.Write(" ");
-                    }
-                    Console.Write(alfabetoArray[i]);
-                    if (i > 0)
-                    {
-                        for (int j = 0; j < 2 * i - 1; j++)
-                        {
-                            Console.Write(" ");
-                        }
-                        Console.Write(alfabetoArray[i]);
-
-                    }
-                    Console.WriteLine();
-                }
-                #endregion
-
-                // Mesma lógica, inversa do topo
-                #region Base do diamante de letras
-                for (int i = letraIndex - 1; i >= 0; i--)
-                {
-                    for (int j = 0; j < letraIndex - i; j++)
-                    {
-                        Console.Write(" ");
-
-                    }
-                    Console.Write(alfabetoArray[i]);
-
-                    if (i > 0)
-                    {
-                        for (int j = 0; j < 2 * i - 1; j++)
-                        {
-                            Console.Write(" ");
-                        }
-                            Console.Write(alfabetoArray[i]);
-                    }
-                    Console.WriteLine();
-                }
-                #endregion
-
+                Console.WriteLine("Aqui eu tentarei utilizar a metodologia que o Thiago comentou!\nUtilizando os valores dos tipos Char.");
 
             }
 
