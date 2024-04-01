8cfeda2a4d3b348333e5941a6d606145c6c487c7
Leonardo Rodrigues <lthkrieger@gmail.com>
2024-04-01 02:55:17 -0300

    Added char calc

--- Diamante-de-letras-ConsoleApp/Program.cs
+++ Diamante-de-letras-ConsoleApp/Program.cs
@@ -1,15 +1,22 @@
-﻿namespace Diamante_De_Letras_ConsoleApp
+﻿namespace iamante_De_Letras_ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             {
-                Console.WriteLine("Diamante de Letras | Academia de Programação 2024!\n");
+                //lista ASCII   A = 65 ~~ Z == 90
+                Console.WriteLine("Diamante de Letras 2.0 | Academia de Programação 2024!\n");
 
-                Console.WriteLine("Aqui eu tentarei utilizar a metodologia que o Thiago comentou!\nUtilizando os valores dos tipos Char.");
-
+                Console.WriteLine("Utilizando os valores dos tipos Char.\n\n");
+                while (true)
+                {
+                    dadosDoDiamante();
+                    if (desejaContinuar("Deseja continuar? (S / N)")) break;
+                }
             }
+
+
 
 
             static tipo obterValor<tipo>(string texto)
@@ -27,6 +34,72 @@
                     return obterValor<tipo>(texto);
                 }
             }
+
+            static bool desejaContinuar(string texto)
+            {
+                Console.WriteLine(texto);
+                string decisao = Console.ReadLine().ToUpper();
+                Console.Clear();
+                return decisao == "N";
+            }
+
+            static void dadosDoDiamante()
+            {
+                char letra = obterValor<char>("Digite uma letra para gerar o diamante: ");
+
+                //char letra = 'Z';
+
+                int letraAscii = Math.Abs(Convert.ToInt32(letra));
+
+                string alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                char[] arrayAlfabeto = alfabeto.ToCharArray(0, alfabeto.Length);
+                int A = letra - 64;
+                construtorDiamante(arrayAlfabeto, A);
+            }
+        }
+
+        private static void construtorDiamante(char[] arrayAlfabeto, int A)
+        {
+            #region Topo do Diamante de Letras
+            for (int i = 0; i < A; i++)
+            {
+                for (int j = 0; j < A - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(arrayAlfabeto[i]);
+                if (i > 0)
+                {
+                    for (int j = 0; j < 2 * i - 1; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(arrayAlfabeto[i]);
+                }
+                Console.WriteLine();
+            }
+            #endregion
+
+            #region Base do Diamante de Letras
+
+            for (int i = A - 2; i >= 0; i--)
+            {
+                for (int j = 0; j <= A - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                if (i > 0)
+                {
+                    Console.Write(arrayAlfabeto[i]);
+                    for (int j = 0; j < 2 * i - 1; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine(arrayAlfabeto[i]);
+            }
+            #endregion
         }
     }
 }
